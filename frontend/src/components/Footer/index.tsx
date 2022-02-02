7a0738fd621710220b16cc902618f363ab685af5
Bruno Lima <brunolima.assuncao1@gmail.com>
2022-02-02 10:34:23 -0400

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/brunolimabr" target="_blank" rel="noreferrer">BRUNO LIMA</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola Bruno sabe tudo: <a href="https://instagram.com/brunolimabr" target="_blank" rel="noreferrer">@brunolimabr</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
